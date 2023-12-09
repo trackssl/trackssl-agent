694a1a56369a99a6cc89e06b8a58717e9df47824
Andrew Libby <alibby@andylibby.org>
2023-12-09 06:45:28 -0500

    Add some logging when fetching list of domains

--- app/services/domains_retriever.rb
+++ app/services/domains_retriever.rb
@@ -1,18 +1,21 @@
 class DomainsRetriever
   include HttpClient
+  include Logging
 
   def self.call
     new.call
   end
 
   def call
+    info "Retrieving domains"
     Domain.transaction do
       new_domains.each(&Domain.method(:create!))
     end
+    info "Retrived #{new_domains.count} domains"
   end
 
   def new_domains
-    @new_domains ||= JSON.parse(response.body).dig('data')
+    @new_domains ||= JSON.parse(response.body).dig('data') || []
   end
 
   def response
